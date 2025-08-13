05dc747e3c971d0a8be65589341682aa809f3043
Scott Mallon <scott@scottmallon.com>
2025-08-13 11:07:39 -0400

    Selected File Name

--- client/src/app/components/fileUpload/FileUpload.tsx
+++ client/src/app/components/fileUpload/FileUpload.tsx
@@ -4,10 +4,11 @@ import React, { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 
 export const FileUpload = () => {
+  const [fileName, setFileName] = React.useState<string | null>(null);
   const onDrop = useCallback((acceptedFiles: File[]) => {
     acceptedFiles.forEach((file) => {
       const reader = new FileReader();
-
+      setFileName(file.name);
       reader.onabort = () => console.log("file reading was aborted");
       reader.onerror = () => console.log("file reading has failed");
       reader.onload = () => {
@@ -21,9 +22,16 @@ export const FileUpload = () => {
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   return (
-    <div className="p-10 m-20 border-dashed border-2" {...getRootProps()}>
-      <input {...getInputProps()} />
-      <div className="text-center">Upload Receipt PDF</div>
+    <div className="p-10 m-20">
+      <div className="border-dashed border-2 p-10" {...getRootProps()}>
+        <input {...getInputProps()} />
+        <div className="text-center m-0-auto">Upload Receipt PDF</div>
+      </div>
+      {fileName && (
+        <div className="mt-4 text-sm text-gray-600">
+          Selected file: <strong>{fileName}</strong>
+        </div>
+      )}
     </div>
   );
 };
