84e7bf685f54031541a3d3f457e931a0b2cb50ee
Scott Mallon <scott@scottmallon.com>
2025-08-13 11:10:46 -0400

    Clear File

--- client/src/app/components/fileUpload/FileUpload.tsx
+++ client/src/app/components/fileUpload/FileUpload.tsx
@@ -5,6 +5,9 @@ import { useDropzone } from "react-dropzone";
 
 export const FileUpload = () => {
   const [fileName, setFileName] = React.useState<string | null>(null);
+  const [fileContent, setFileContent] = React.useState<ArrayBuffer | null>(
+    null
+  );
   const onDrop = useCallback((acceptedFiles: File[]) => {
     acceptedFiles.forEach((file) => {
       const reader = new FileReader();
@@ -14,6 +17,7 @@ export const FileUpload = () => {
       reader.onload = () => {
         // Do whatever you want with the file contents
         const binaryStr = reader.result;
+        setFileContent(binaryStr as ArrayBuffer);
         console.log(binaryStr);
       };
       reader.readAsArrayBuffer(file);
@@ -30,6 +34,15 @@ export const FileUpload = () => {
       {fileName && (
         <div className="mt-4 text-sm text-gray-600">
           Selected file: <strong>{fileName}</strong>
+          <button
+            className="ml-4 text-blue-500 hover:underline"
+            onClick={() => {
+              setFileName(null);
+              setFileContent(null);
+            }}
+          >
+            Clear File
+          </button>
         </div>
       )}
     </div>
